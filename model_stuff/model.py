0a8d3bc5db9f8da7c7044af17c2d3957993f9f94
D-SehKim <123012677+D-SehKim@users.noreply.github.com>
2023-09-23 16:38:22 -0400

    Created model and compiled it

--- model_stuff/model.py
+++ model_stuff/model.py
@@ -1,1 +1,14 @@
+from tensorflow import keras
+from keras import layers, Sequential
+
+model = Sequential([
+    layers.LSTM(512, activation='sigmoid', return_sequences=True),
+    layers.Dropout(0.2),
+    layers.LSTM(128, activation='sigmoid', return_sequences=True),
+    layers.LSTM(64),
+    layers.Dropout(0.2),
+    layers.Dense(3, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
