3d23a4176bb8425887c01d9a83990b3227b4da7b
D-SehKim <123012677+D-SehKim@users.noreply.github.com>
2023-09-23 15:56:29 -0400

    Added list of Mozart song links in list format

---
+++ composer_list.py
@@ -0,0 +1,23 @@
+mozart_list = [
+    "http://www.piano-midi.de/midis/mozart/mz_311_1.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_311_2.mid"
+    "http://www.piano-midi.de/midis/mozart/mz_311_3.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_330_1.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_330_2.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_330_3.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_331_1.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_331_2.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_331_3.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_332_1.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_332_2.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_332_3.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_333_1.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_333_2.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_333_3.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_545_1.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_545_2.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_545_3.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_570_1.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_570_2.mid",
+    "http://www.piano-midi.de/midis/mozart/mz_570_3.mid"
+] 
